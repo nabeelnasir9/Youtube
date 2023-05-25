43f7f2c3bfff1b85439d2bc3ede02be904c8cd03
nabeelnasir9 <nasirnabeel36@gmail.com>
2023-05-26 02:02:07 +0500

    update 2.0

--- my_youtube/src/components/Feed.jsx
+++ my_youtube/src/components/Feed.jsx
@@ -1,8 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import {Box, Stack, Typography} from '@mui/material';
 import {Sidebar, Videos} from './'
+import { fetchFromApi } from '../utilis/fetchFromAPI';
 
 const Feed = () => {
+useEffect(()=>{
+
+},[]);
+
   return (
     <Stack sx={{flexDirection:{sx:'column', md:'row'}}}>
       <Box sx={{height:{sx:'auto',md:'92vh'},borderRight:'1px solid #3d3d3d',px:{sx:'0', md:'2'}}}>
